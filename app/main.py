36f05e1db58f0e91176093b77d81285712432ffe
igorkorobko <nekto.korobko@gmail.com>
2026-08-27 14:07:55 +0300

    feat: add root endpoint

--- app/main.py
+++ app/main.py
@@ -3,6 +3,14 @@ from fastapi import FastAPI
 app = FastAPI()
 
 
+@app.get("/")
+async def main():
+    return {
+        "service": "supportops",
+        "message": "SupportOps Hub is running"
+    }
+
+
 @app.get("/health")
 async def health():
     return {
